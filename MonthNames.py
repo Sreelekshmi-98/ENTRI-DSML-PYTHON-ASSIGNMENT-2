53b97bf2efe128331e07fa450c08eb2d77fe75f8
Sreelekshmi-98 <sreekuttylekshmisree@gmail.com>
2024-10-15 20:22:42 +0530

    Add files via upload

---
+++ MonthNames.py
@@ -0,0 +1,29 @@
+month_number = int(input("Enter the month (1-12): "))
+
+if month_number == 1:
+    print("Month 1 is January")
+elif month_number == 2:
+    print("Month 2 is February")
+elif month_number == 3:
+    print("Month 3 is March")
+elif month_number == 4:
+    print("Month 4 is April")
+elif month_number == 5:
+    print("Month 5 is May")
+elif month_number == 6:
+    print("Month 6 is June")
+elif month_number == 7:
+    print("Month 7 is July")
+elif month_number == 8:
+    print("Month 8 is August")
+elif month_number == 9:
+    print("Month 9 is September")
+elif month_number == 10:
+    print("Month 10 is October")
+elif month_number == 11:
+    print("Month 11 is November")
+elif month_number == 12:
+    print("Month 12 is December")
+else:
+    print("Invalid input! Please enter a number between 1 and 12.")
+
